292ad6b25ae4ed681e0a2521a2a3b02fac187109
portellaandrey <andreyportella.parana@gmail.com>
2025-12-09 10:34:48 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const formulario = document.getElementById('formulario');
+    
+    const seuNumeroWhatsapp = "5541991850421"; // número completo com código do país
+    
+    formulario.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        const nome = formulario.querySelector('input[name="nome"]').value.trim();
+        const mensagem = formulario.querySelector('textarea[name="mensagem"]').value.trim();
+        
+        if (!nome | !mensagem) {
+            alert('Por favor, preencha todos os campos do formulário.');
+            return;
+        }
+        
+        const textoMensagem = encodeURIComponent(
+            `Olá Andrey, meu nome é ${nome}\n\n` +
+            `Mensagem:\n${mensagem}`
+        );
+        
+        const whatsappLink = `https://wa.me/${seuNumeroWhatsapp}?text=${textoMensagem}`;
+        
+        window.open(whatsappLink, '_blank', 'noopener,noreferrer');
+        
+        formulario.reset();
+    });
+});
